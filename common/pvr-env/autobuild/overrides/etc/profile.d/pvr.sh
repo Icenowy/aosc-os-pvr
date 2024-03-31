8cec109235bcf3ccde900ab470def324aa28e089
Icenowy Zheng <uwu@icenowy.me>
2024-03-31 19:03:30 +0800

    pvr-env: fix json file name
    Signed-off-by: Icenowy Zheng <uwu@icenowy.me>

--- common/pvr-env/autobuild/overrides/etc/profile.d/pvr.sh
+++ common/pvr-env/autobuild/overrides/etc/profile.d/pvr.sh
@@ -1,3 +1,3 @@
-export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/50_pvr.json
+export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/40_pvr.json
 export KWIN_COMPOSE=O2ES
 export QT_XCB_GL_INTEGRATION=xcb_egl
